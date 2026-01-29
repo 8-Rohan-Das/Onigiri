04233628a69e75cadc091713ac26745a44763418
Ankit <ak4347685@gmail.com>
2026-01-29 15:11:05 +0530

    Resolved merge conflicts and updated theme colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
 
 // Import pages
 import LoginPage from './pages/auth/LoginPage';
@@ -11,22 +14,43 @@ import MessagesPage from './pages/MessagesPage';
 import OrderHistoryPage from './pages/OrderHistoryPage';
 import OthersPage from './pages/OthersPage';
 
+
+
 function App() {
+
   return (
+
     <BrowserRouter>
+
       <Routes>
+
         <Route path="/" element={<LoginPage />} />
+
         <Route path="/login" element={<LoginPage />} />
+
         <Route path="/signup" element={<SignUpPage />} />
+
         <Route path="/home" element={<Homepage />} />
+
         <Route path="/category/:category" element={<CategoryPage />} />
+
         <Route path="/favorite" element={<FavoritePage />} />
+
         <Route path="/messages" element={<MessagesPage />} />
+
         <Route path="/order-history" element={<OrderHistoryPage />} />
+
         <Route path="/others" element={<OthersPage />} />
+
       </Routes>
+
     </BrowserRouter>
+
   );
+
 }
 
+
+
 export default App;
+
